1d1d9a0781ff1d6f90958e5f959b65e9ac95e9f6
Yunfan Zhang <yunfanz@berkeley.edu>
2017-04-05 20:10:20 -0700

    save

--- find_HII_bubbles.py
+++ find_HII_bubbles.py
@@ -2,11 +2,14 @@ from Choud14 import *
 from tocmfastpy import *
 from IO_utils import *
 import pycuda.compiler as nvcc
-import pycuda.gpuarray as gpu
+import pycuda.gpuarray as gpuarray
 import pycuda.driver as cu
 import pycuda.autoinit
+from pyfft.cuda import Plan
+from pycuda.tools import make_default_context
 
-def find_bubbles(I):
+def find_bubbles(I, fil='kspace'):
+	"""brute force method"""
 	zeta = 40.
 	Z = 12.
 	RMAX = 3.
@@ -24,7 +27,10 @@ def find_bubbles(I):
         'ZETA': zeta
     }
 	main_module = nvcc.SourceModule(kernel_code)
-	real_tophat_kernel = main_module.get_function("real_tophat_kernel")
+	if fil == 'rspace':
+		kernel = main_module.get_function("real_tophat_kernel")
+	elif fil == 'kspace':
+		kernel = main_module.get_function("k_tophat_kernel")
 	image_texture = main_module.get_texref("img")
 
 	# Get contiguous image + shape.
@@ -42,7 +48,7 @@ def find_bubbles(I):
 	width         = np.int32(width)
 
 	# Transfer labels asynchronously.
-	ionized_d = gpu.to_gpu_async(ionized)
+	ionized_d = gpuarray.to_gpu_async(ionized)
 	I_cu = cu.np_to_array(I, order='C')
 	cu.bind_array_to_texref(I_cu, image_texture)
 
@@ -55,7 +61,98 @@ def find_bubbles(I):
 		start = cu.Event()
 		end = cu.Event()
 		start.record()
-		real_tophat_kernel(ionized_d, width, R, S0, block=block_size, grid=grid_size)
+		kernel(ionized_d, width, R, S0, block=block_size, grid=grid_size)
+		end.record()
+		end.synchronize()
+		R *= (1./1.5)
+
+	ionized = ionized_d.get()
+	return ionized
+
+def conv(delta_d, filt_d, shape, fil):
+	smoothI = np.zeros(shape, dtype=np.complex64)
+	smoothed_d = gpuarray.to_gpu(smoothI)
+	plan = Plan(shape, dtype=np.complex64)
+	plan.execute(delta_d)
+	if fil == 'rspace':
+		plan.execute(filt_d)
+	smoothed_d = delta_d * filt_d.conj()
+	plan.execute(smoothed_d, inverse=True)
+	return smoothed_d.real
+
+def conv_bubbles(I, fil='kspace'):
+	"""uses fft convolution"""
+	zeta = 40.
+	Z = 12.
+	RMAX = 3.
+	RMIN = 1.
+	mm = mmin(Z)
+	smin = sig0(m2R(mm))
+	deltac = Deltac(Z)
+	fgrowth = deltac/1.686
+	"""find bubbbles for deltax box I"""
+	kernel_source = open("find_bubbles.cu").read()
+	kernel_code = kernel_source % {
+        'DELTAC': deltac,
+        'RMIN': RMIN,
+        'SMIN': smin, 
+        'ZETA': zeta
+    }
+	main_module = nvcc.SourceModule(kernel_code)
+	update_kernel = main_module.get_function("update_kernel")
+	real_tophat = main_module.get_function("real_tophat")
+	k_tophat = main_module.get_function("k_tophat")
+	image_texture = main_module.get_texref("img")
+
+	# Get contiguous image + shape.
+	height, width, depth = I.shape
+	
+	
+	# Get block/grid size for steps 1-3.
+	block_size =  (8,8,8)
+	grid_size =   (width/(block_size[0]-2)+1,
+				height/(block_size[0]-2)+1,
+				depth/(block_size[0]-2)+1)
+	 # Initialize variables.
+	ionized       = np.zeros([height,width,depth]) 
+	ionized       = np.float32(ionized)
+	width         = np.int32(width)
+	I             = np.float32(I.copy()*fgrowth)
+	smoothI       = np.zeros(I.shape, dtype=np.complex64)
+	filt          = np.ones_like(I)
+
+
+	# Transfer labels asynchronously.
+	ionized_d = gpuarray.to_gpu_async(ionized)
+	delta_d = gpuarray.to_gpu_async(I)
+	# I_cu = cu.np_to_array(I, order='C')
+	# cu.bind_array_to_texref(I_cu, image_texture)
+
+	plan = Plan(I.shape, dtype=np.complex64)
+	R = RMAX
+	while R > RMIN:
+		print R
+		R = np.float32(R)
+
+		S0 = np.float32(sig0(R))
+
+		start = cu.Event()
+		step1 = cu.Event()
+		step2 = cu.Event()
+		end = cu.Event()
+
+		start.record()
+		filt_d = gpuarray.to_gpu(filt)
+		if fil == 'kspace':
+			ks = (9*np.pi/2)**(1./3)/R
+			k_tophat(filt_d, width, ks, block=block_size, grid=grid_size)
+		elif fil == 'rspace':
+			real_tophat(filt_d, width, R, block=block_size, grid=grid_size)
+		step1.record(); step1.synchronize()
+		smoothed_d = conv(delta_d.astype(np.complex64), filt_d.astype(np.complex64), I.shape, fil=fil)
+		import IPython; IPython.embed()
+		step2.record(); step2.synchronize()
+		update_kernel(ionized_d, smoothed_d, width, R, S0, block=block_size, grid=grid_size)
 		end.record()
 		end.synchronize()
 		R *= (1./1.1)
@@ -74,6 +171,6 @@ if __name__ == '__main__':
 	b1 = boxio.readbox(file)
 	d1 = 1 - b1.box_data[::10, ::10, ::10]
 	print d1.shape
-	ion_field = find_bubbles(d1)
+	ion_field = find_bubbles(d1, fil='rspace')
 	print ion_field.shape
 	import IPython; IPython.embed()
--- find_bubbles.cu
+++ find_bubbles.cu
@@ -37,5 +37,98 @@ __global__ void real_tophat_kernel(float* ionized, const int w, float R, float S
   float fcoll = 1 - erf((deltac - delta0)/sqrt(2*(smin - S0)));
   //ionized[p] = fcoll* %(ZETA)s;;
   if (fcoll >= 1/%(ZETA)s) ionized[p] = 1.0;
-  else if (R==%(RMIN)s) { ionized[p] = fcoll * %(ZETA)s; }
+  else { ionized[p] = fcoll * %(ZETA)s; }
+ }
+ __global__ void k_tophat_kernel(float* ionized, const int w, float R, float S0)
+{
+	int tx = threadIdx.x;  int ty = threadIdx.y; int tz = threadIdx.z;
+	int bx = blockIdx.x;   int by = blockIdx.y; int bz = blockIdx.z;
+	int bdx = blockDim.x;  int bdy = blockDim.y; int bdz = blockDim.z;
+	int i = bdx * bx + tx; int j = bdy * by + ty; int k = bdz * bz + tz;
+	int p = INDEX(k,j,i,w);
+	float ks = pow((9*3.14159/2),1.0/3) / R;
+	if (j >= w || i >= w || k >= w || ionized[p] == 1) return;
+
+	float rsq, r, y;
+	float deltasum = 0;
+	float deltac = %(DELTAC)s;
+	float smin = %(SMIN)s;
+	float count = 0;
+	float W;
+	for (int kk = 0; kk < w; kk++) {
+		for (int jj = 0; jj < w; jj++) {
+			for (int ii = 0; ii < w; ii++){
+				rsq = (ii-i)*(ii-i)+(jj-j)*(jj-j)+(kk-k)*(kk-k);
+				r = pow( rsq, float(0.5) );
+				y = ks*r;
+				W = (sin(y) - y*cos(y))/( 2*pow(3.14159,2)*pow(r,3) );
+				deltasum += W * tex3D(img,i,j,k);
+				count += W;
+			}
+		}
+	}
+	float delta0 = deltasum/count;
+	float fcoll = 1 - erf((deltac - delta0)/sqrt(2*(smin - S0)));
+	//ionized[p] = fcoll* %(ZETA)s;;
+	if (fcoll >= 1/%(ZETA)s) ionized[p] = 1.0;
+	else { ionized[p] = fcoll * %(ZETA)s; }
+ }
+
+
+__global__ void real_tophat(float* filter, int w, float R)
+{
+	int tx = threadIdx.x;  int ty = threadIdx.y; int tz = threadIdx.z;
+	int bx = blockIdx.x;   int by = blockIdx.y; int bz = blockIdx.z;
+	int bdx = blockDim.x;  int bdy = blockDim.y; int bdz = blockDim.z;
+	int i = bdx * bx + tx; int j = bdy * by + ty; int k = bdz * bz + tz;
+	int p = INDEX(k,j,i,w);
+	if (j >= w || i >= w || k >= w) return;
+	float rsq = (w/2-i)*(w/2-i)+(w/2-j)*(w/2-j)+(w/2-k)*(w/2-k);
+	float vol = 4.0*3.1415926*R*R*R/3.0;
+	if (rsq < R*R)
+	{
+		filter[p] = 1./vol;
+	}
+	else
+	{
+		filter[p] = 0;
+	}
+ }
+
+ __global__ void k_tophat(float* filter, int w, float ks)
+{
+	int tx = threadIdx.x;  int ty = threadIdx.y; int tz = threadIdx.z;
+	int bx = blockIdx.x;   int by = blockIdx.y; int bz = blockIdx.z;
+	int bdx = blockDim.x;  int bdy = blockDim.y; int bdz = blockDim.z;
+	int i = bdx * bx + tx; int j = bdy * by + ty; int k = bdz * bz + tz;
+	int p = INDEX(k,j,i,w);
+	if (j >= w || i >= w || k >= w) return;
+	float ksq = i*i + j*j + k*k;
+	float vol = 4.0*3.1415926*ks*ks*ks/3.0;
+	if (ksq < ks*ks)
+	{
+		filter[p] = 1./vol;
+	}
+	else
+	{
+		filter[p] = 0;
+	}
+ }
+
+__global__ void update_kernel(float* ionized, float* smoothed, const int w, float R, float S0)
+{
+  int tx = threadIdx.x;  int ty = threadIdx.y; int tz = threadIdx.z;
+  int bx = blockIdx.x;   int by = blockIdx.y; int bz = blockIdx.z;
+  int bdx = blockDim.x;  int bdy = blockDim.y; int bdz = blockDim.z;
+  int i = bdx * bx + tx; int j = bdy * by + ty; int k = bdz * bz + tz;
+  int p = INDEX(k,j,i,w);
+  if (j >= w || i >= w || k >= w || ionized[p] == 1) return;
+
+  float delta0 = smoothed[p];
+  float deltac = %(DELTAC)s;
+  float smin = %(SMIN)s;
+  float fcoll = 1 - erf((deltac - delta0)/sqrt(2*(smin - S0)));
+  ionized[p] = fcoll;
+  //if (fcoll >= 1/%(ZETA)s) ionized[p] = 1.0;
+  //else if (R==%(RMIN)s) { ionized[p] = fcoll * %(ZETA)s; }
  }
