402c6d87263e92b55d47a001c1ca03293b28f91f
Yunfan Zhang <yunfanz@berkeley.edu>
2017-04-06 20:20:50 -0700

    save

--- find_HII_bubbles.py
+++ find_HII_bubbles.py
@@ -8,11 +8,11 @@ import pycuda.autoinit
 from pyfft.cuda import Plan
 from pycuda.tools import make_default_context
 
-def find_bubbles(I, fil='kspace'):
+def find_bubbles(I, scale=1., fil='kspace'):
 	"""brute force method"""
 	zeta = 40.
 	Z = 12.
-	RMAX = 3.
+	RMAX = 30.
 	RMIN = 1.
 	mm = mmin(Z)
 	smin = sig0(m2R(mm))
@@ -56,12 +56,12 @@ def find_bubbles(I, fil='kspace'):
 	R = RMAX
 	while R > RMIN:
 		print R
-		R = np.float32(R)
+		Rpix = np.float32(R/scale)
 		S0 = np.float32(sig0(R))
 		start = cu.Event()
 		end = cu.Event()
 		start.record()
-		kernel(ionized_d, width, R, S0, block=block_size, grid=grid_size)
+		kernel(ionized_d, width, Rpix, S0, block=block_size, grid=grid_size)
 		end.record()
 		end.synchronize()
 		R *= (1./1.5)
@@ -80,32 +80,42 @@ def conv(delta_d, filt_d, shape, fil):
 	plan.execute(smoothed_d, inverse=True)
 	return smoothed_d.real
 
-def conv_bubbles(I, fil='kspace'):
+def conv_bubbles(I, param_dict, scale=None, fil=1, visualize=False):
 	"""uses fft convolution"""
 	zeta = 40.
-	Z = 12.
-	RMAX = 3.
-	RMIN = 1.
+	Lfactor = 0.620350491
+	Z = param_dict['z']
+	DELTA_R_FACTOR = 1.1
+	
+	if scale is None:
+		scale = param_dict['BoxeSize']/param_dict['HIIdim']
+	dk = 2*np.pi/I.shape[0]*scale#param_dict['BoxSize'] #delta k in inverse Mpc
+	RMAX = np.float32(30.) #in Mpc
+	RMIN = np.float32(1.)
 	mm = mmin(Z)
 	smin = sig0(m2R(mm))
+	#smin = pb.sigma_r(m2R(mm), Z, **cosmo)[0]
 	deltac = Deltac(Z)
 	fgrowth = deltac/1.686
+	fc_mean_ps = pb.collapse_fraction(np.sqrt(smin), deltac).astype(np.float32)  #mean collapse fraction of universe
+	print fc_mean_ps
 	"""find bubbbles for deltax box I"""
 	kernel_source = open("find_bubbles.cu").read()
 	kernel_code = kernel_source % {
         'DELTAC': deltac,
         'RMIN': RMIN,
         'SMIN': smin, 
-        'ZETA': zeta
+        'ZETA': zeta,
+        'DELTAK': dk
     }
 	main_module = nvcc.SourceModule(kernel_code)
+	fcoll_kernel = main_module.get_function("fcoll_kernel")
 	update_kernel = main_module.get_function("update_kernel")
-	real_tophat = main_module.get_function("real_tophat")
-	k_tophat = main_module.get_function("k_tophat")
-	image_texture = main_module.get_texref("img")
-
+	final_kernel = main_module.get_function("final_kernel")
+	HII_filter = main_module.get_function("HII_filter")
 	# Get contiguous image + shape.
 	height, width, depth = I.shape
+	HII_TOT_NUM_PIXELS = height*width*depth
 	
 	
 	# Get block/grid size for steps 1-3.
@@ -117,9 +127,8 @@ def conv_bubbles(I, fil='kspace'):
 	ionized       = np.zeros([height,width,depth]) 
 	ionized       = np.float32(ionized)
 	width         = np.int32(width)
-	I             = np.float32(I.copy()*fgrowth)
-	smoothI       = np.zeros(I.shape, dtype=np.complex64)
-	filt          = np.ones_like(I)
+	I             = np.float32(I.copy()*fgrowth) #linearly extrapolate the non-linear density to present?
+	#filt          = np.ones_like(I)
 
 
 	# Transfer labels asynchronously.
@@ -128,13 +137,41 @@ def conv_bubbles(I, fil='kspace'):
 	# I_cu = cu.np_to_array(I, order='C')
 	# cu.bind_array_to_texref(I_cu, image_texture)
 
-	plan = Plan(I.shape, dtype=np.complex64)
+	fftplan = Plan(I.shape, dtype=np.complex64)
 	R = RMAX
-	while R > RMIN:
-		print R
+
+	if visualize:
+		fig = plt.figure(figsize=(12,12))
+		ax = fig.add_subplot(111)
+		fig.suptitle(" PyCUDA")
+		ax.set_title('title')
+		plt.subplot(121)
+		mydelta = plt.imshow(delta_d.get().real[width/2])
+		plt.colorbar()
+		plt.subplot(122)
+		myion = plt.imshow(np.ones_like(I)[width/2])
+		plt.colorbar()
+		plt.pause(.01)
+		plt.draw()
+		#plt.colorbar()
+	final_step = False
+	final_denom = 1
+	temparg = 2*(smin - sig0(Lfactor*scale) )
+	if temparg < 0:
+		raise(Exception)
+	else:
+		final_denom = np.sqrt(temparg).astype(np.float32)
+	while not final_step:
+		print 'R={} Mpc'.format(R)
+		if (R/DELTA_R_FACTOR) <= (Lfactor*scale) or ((R/DELTA_R_FACTOR) <= RMIN): #stop if reach either rmin or cell size
+			final_step = True
 		R = np.float32(R)
+		Rpix = np.float32(R/scale)
 
 		S0 = np.float32(sig0(R))
+		#S0 = np.float32(pb.sigma_r(R, Z, **cosmo)[0])
+		denom = np.sqrt(2*(smin - S0)).astype(np.float32)
+		print 'denom', denom
 
 		start = cu.Event()
 		step1 = cu.Event()
@@ -142,20 +179,44 @@ def conv_bubbles(I, fil='kspace'):
 		end = cu.Event()
 
 		start.record()
-		filt_d = gpuarray.to_gpu(filt)
-		if fil == 'kspace':
-			ks = (9*np.pi/2)**(1./3)/R
-			k_tophat(filt_d, width, ks, block=block_size, grid=grid_size)
-		elif fil == 'rspace':
-			real_tophat(filt_d, width, R, block=block_size, grid=grid_size)
+		#smoothed_d = conv(delta_d.astype(np.complex64), I.shape, fil=fil)
+
+		delta_d = gpuarray.to_gpu_async(I.astype(np.complex64))
+		fcoll_d = gpuarray.zeros(I.shape, dtype=np.float32)
+		fftplan.execute(delta_d)
 		step1.record(); step1.synchronize()
-		smoothed_d = conv(delta_d.astype(np.complex64), filt_d.astype(np.complex64), I.shape, fil=fil)
-		import IPython; IPython.embed()
+		
+		HII_filter(delta_d, width, np.int32(fil), R, block=block_size, grid=grid_size)
+
+		#import IPython; IPython.embed()
+		fftplan.execute(delta_d, inverse=True)
+
+		# import IPython; IPython.embed()
+
 		step2.record(); step2.synchronize()
-		update_kernel(ionized_d, smoothed_d, width, R, S0, block=block_size, grid=grid_size)
+		if not final_step:
+			fcoll_kernel(fcoll_d, delta_d.real, width, denom, block=block_size, grid=grid_size)
+			fcollmean = gpuarray.sum(fcoll_d).get()/float(HII_TOT_NUM_PIXELS)
+			fcoll_d *= fc_mean_ps/fcollmean# #normalize since we used non-linear density
+			#print fcoll_d.dtype
+			update_kernel(ionized_d, fcoll_d, width, block=block_size, grid=grid_size)
+		else:
+			print 'final denom', final_denom
+			fcoll_kernel(fcoll_d, delta_d.real, width, final_denom, block=block_size, grid=grid_size)
+			fcollmean = gpuarray.sum(fcoll_d).get()/float(HII_TOT_NUM_PIXELS)
+			fcoll_d *= fc_mean_ps/fcollmean
+			final_kernel(ionized_d, fcoll_d, width, block=block_size, grid=grid_size)
 		end.record()
 		end.synchronize()
-		R *= (1./1.1)
+		if visualize:
+			mydelta.set_data(delta_d.real.get()[width/2])
+			myion.set_data(ionized_d.get()[width/2])
+			ax.set_title('R = %f'%(R))
+			plt.pause(.01)
+			plt.draw()
+
+
+		R = R/DELTA_R_FACTOR
 
 	ionized = ionized_d.get()
 	return ionized
@@ -169,8 +230,10 @@ if __name__ == '__main__':
 	files = find_deltax(opts.DIR, z=z)
 	file = files[0]
 	b1 = boxio.readbox(file)
-	d1 = 1 - b1.box_data[::10, ::10, ::10]
+	scale = 1
+	#d1 = 1 - b1.box_data[::scale, ::scale, ::scale]
+	d1 = b1.box_data#[:256, :256, :256]
 	print d1.shape
-	ion_field = find_bubbles(d1, fil='rspace')
+	ion_field = conv_bubbles(d1, b1.param_dict, scale=float(scale), fil=1, visualize=False)
 	print ion_field.shape
 	import IPython; IPython.embed()
